0ff9c56d39219a6e8872ca40890ee152e69c8c5d
hkd987 <lundinmatthews@me.com>
2018-11-27 13:10:49 -0600

    Disable camel case rules fo this object

--- test/mockAuthResult.test.js
+++ test/mockAuthResult.test.js
@@ -1,21 +1,16 @@
 /* eslint-env node, jest */
+/*eslint camelcase: ["error", {properties: "never"}]*/
 import { mockAuthResult } from './mockAuthResult'
 
 it('matches if the actual obejct does not change', () => {
   expect(mockAuthResult).toEqual(
     expect.objectContaining({
-        // eslint-disable-next-line
-        expires_in: expect.any(Number),
-        // eslint-disable-next-line
-        access_token: expect.any(String),
-        // eslint-disable-next-line
-        id_token: expect.any(String),
-        // eslint-disable-next-line
-        scope: expect.any(String),
-        // eslint-disable-next-line
-        first_issued_at: expect.any(Number),
-        // eslint-disable-next-line
-        expires_at: expect.any(Number) 
+      expires_in: expect.any(Number),
+      access_token: expect.any(String),
+      id_token: expect.any(String),
+      scope: expect.any(String),
+      first_issued_at: expect.any(Number),
+      expires_at: expect.any(Number)
     })
   )
 })
