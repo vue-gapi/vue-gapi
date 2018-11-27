45db77731c5547cf1e70eb9fe86d5178f640c5c1
hkd987 <lundinmatthews@me.com>
2018-11-27 12:25:22 -0600

    cleaned up jest testing page still working on it

--- test/GoogleAuthService.test.js
+++ test/GoogleAuthService.test.js
@@ -1,18 +1,21 @@
+/* eslint-env node, jest */
 // https://developers.google.com/identity/sign-in/web/reference#gapiauth2authresponse
 import AuthService from '../src/VueGAPI/GoogleAuthService'
 jest.mock('../src/VueGAPI/GoogleAuthService')
 
+/*
 const mockAuthResult = {
-  expires_in: 123456,
+  expires_in: 123456
 }
+*/
 
-/* eslint-env node, jest */
 beforeEach(() => {
   // Clear all instances and calls to constructor and all methods:
   AuthService.mockClear()
 })
 
 it('Can be mounted', () => {
+  // eslint-disable-next-line
   const newService = new AuthService()
   expect(AuthService).toHaveBeenCalledTimes(1)
 })
