51c529f55da0b70fb928a932cbd5456b6e8808ce
Cedric Poilly <cedric.poilly@theredpandas.com>
2020-04-16 01:20:24 +0400

    Grant offline access. Store refresh token

--- src/VueGAPI/GoogleAuthService.js
+++ src/VueGAPI/GoogleAuthService.js
@@ -1,17 +1,17 @@
 export default class GoogleAuthService {
-  constructor () {
-    this.authenticated = this.isAuthenticated()
-    this.authInstance = null
+  constructor() {
+    this.authenticated = this.isAuthenticated();
+    this.authInstance = null;
 
-    this.offlineAccessCode = null
-    this.getOfflineAccessCode = this.getOfflineAccessCode.bind(this)
-    this.grantOfflineAccess = this.grantOfflineAccess.bind(this)
-    this.login = this.login.bind(this)
-    this.refreshToken = this.refreshToken.bind(this)
-    this.logout = this.logout.bind(this)
-    this.isAuthenticated = this.isAuthenticated.bind(this)
-    this.isSignedIn = this.isSignedIn.bind(this)
-    this.listenUserSignIn = this.listenUserSignIn.bind(this)
+    this.offlineAccessCode = null;
+    this.getOfflineAccessCode = this.getOfflineAccessCode.bind(this);
+    this.grantOfflineAccess = this.grantOfflineAccess.bind(this);
+    this.login = this.login.bind(this);
+    this.refreshToken = this.refreshToken.bind(this);
+    this.logout = this.logout.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
+    this.isSignedIn = this.isSignedIn.bind(this);
+    this.listenUserSignIn = this.listenUserSignIn.bind(this);
   }
 
   /**
@@ -29,8 +29,8 @@ export default class GoogleAuthService {
    * @returns
    *   a string of when the google auth token expires
    */
-  _expiresAt (authResult) {
-    return JSON.stringify(authResult.expires_in * 1000 + new Date().getTime())
+  _expiresAt(authResult) {
+    return JSON.stringify(authResult.expires_in * 1000 + new Date().getTime());
   }
 
   /**
@@ -50,18 +50,18 @@ export default class GoogleAuthService {
    * @fires localStorage.setItem
    *
    */
-  _setStorage (authResult, profile = null) {
-    localStorage.setItem('gapi.access_token', authResult.access_token)
-    localStorage.setItem('gapi.id_token', authResult.id_token)
-    localStorage.setItem('gapi.expires_at', this._expiresAt(authResult))
+  _setStorage(authResult, profile = null) {
+    localStorage.setItem("gapi.access_token", authResult.access_token);
+    localStorage.setItem("gapi.id_token", authResult.id_token);
+    localStorage.setItem("gapi.expires_at", this._expiresAt(authResult));
 
     if (profile) {
-      localStorage.setItem('gapi.id', profile.getId())
-      localStorage.setItem('gapi.full_name', profile.getName())
-      localStorage.setItem('gapi.first_name', profile.getGivenName())
-      localStorage.setItem('gapi.last_name', profile.getFamilyName())
-      localStorage.setItem('gapi.image_url', profile.getImageUrl())
-      localStorage.setItem('gapi.email', profile.getEmail())
+      localStorage.setItem("gapi.id", profile.getId());
+      localStorage.setItem("gapi.full_name", profile.getName());
+      localStorage.setItem("gapi.first_name", profile.getGivenName());
+      localStorage.setItem("gapi.last_name", profile.getFamilyName());
+      localStorage.setItem("gapi.image_url", profile.getImageUrl());
+      localStorage.setItem("gapi.email", profile.getEmail());
     }
   }
 
@@ -77,75 +77,76 @@ export default class GoogleAuthService {
    * @fires localStorage.removeItem
    *
    */
-  _clearStorage () {
-    localStorage.removeItem('gapi.access_token')
-    localStorage.removeItem('gapi.id_token')
-    localStorage.removeItem('gapi.expires_at')
-    localStorage.removeItem('gapi.id')
-    localStorage.removeItem('gapi.full_name')
-    localStorage.removeItem('gapi.first_name')
-    localStorage.removeItem('gapi.last_name')
-    localStorage.removeItem('gapi.image_url')
-    localStorage.removeItem('gapi.email')
+  _clearStorage() {
+    localStorage.removeItem("gapi.access_token");
+    localStorage.removeItem("gapi.id_token");
+    localStorage.removeItem("gapi.expires_at");
+    localStorage.removeItem("gapi.id");
+    localStorage.removeItem("gapi.full_name");
+    localStorage.removeItem("gapi.first_name");
+    localStorage.removeItem("gapi.last_name");
+    localStorage.removeItem("gapi.image_url");
+    localStorage.removeItem("gapi.email");
   }
 
-  _setOfflineAccessCode (authResult) {
+  _setOfflineAccessCode(authResult) {
     if (authResult.code) {
-      this.offlineAccessCode = authResult.code
+      this.offlineAccessCode = authResult.code;
     } else {
-      throw new Error('Offline access code missing from result', authResult)
+      throw new Error("Offline access code missing from result", authResult);
     }
   }
 
-  _setSession (response) {
-    const profile = this.authInstance.currentUser.get().getBasicProfile()
-    const authResult = this.authInstance.currentUser.get().getAuthResponse(true)
-    this._setStorage(authResult, profile)
-    this.authenticated = true
+  _setSession(response) {
+    const profile = this.authInstance.currentUser.get().getBasicProfile();
+    const authResult = this.authInstance.currentUser
+      .get()
+      .getAuthResponse(true);
+    this._setStorage(authResult, profile);
+    this.authenticated = true;
   }
 
-  getOfflineAccessCode () {
-    return this.offlineAccessCode
+  getOfflineAccessCode() {
+    return this.offlineAccessCode;
   }
 
-  grantOfflineAccess (event) {
-    if (!this.authInstance) throw new Error('gapi not initialized')
-    return this.authInstance.grantOfflineAccess()
-      .then(this._setOfflineAccessCode.bind(this))
+  grantOfflineAccess(event) {
+    if (!this.authInstance) throw new Error("gapi not initialized");
+    return this.authInstance
+      .grantOfflineAccess()
+      .then(this._setOfflineAccessCode.bind(this));
   }
 
-  login (event) {
-    if (!this.authInstance) throw new Error('gapi not initialized')
-    const this$1 = this
+  login(event) {
+    if (!this.authInstance) throw new Error("gapi not initialized");
+    const this$1 = this;
     return new Promise((res, rej) => {
-      this$1.authInstance.signIn()
-        .then(function (response) {
-          this$1._setSession(response)
-          res()
-        })
-    })
+      this$1.authInstance.grantOfflineAccess().then(function (response) {
+        localStorage.setItem("gapi.refresh_token", response.code);
+        this$1._setSession(response);
+        res();
+      });
+    });
   }
 
-  refreshToken (event) {
-    if (!this.authInstance) throw new Error('gapi not initialized')
-    const GoogleUser = this.authInstance.currentUser.get()
-    GoogleUser.reloadAuthResponse()
-      .then((authResult) => {
-        this._setStorage(authResult)
-      })
+  refreshToken(event) {
+    if (!this.authInstance) throw new Error("gapi not initialized");
+    const GoogleUser = this.authInstance.currentUser.get();
+    GoogleUser.reloadAuthResponse().then((authResult) => {
+      this._setStorage(authResult);
+    });
   }
 
-  logout (event) {
-    if (!this.authInstance) throw new Error('gapi not initialized')
-    const this$1 = this
+  logout(event) {
+    if (!this.authInstance) throw new Error("gapi not initialized");
+    const this$1 = this;
     return new Promise((res, rej) => {
-      this$1.authInstance.signOut()
-        .then(function () {
-          this$1._clearStorage()
-          this$1.authenticated = false
-          res()
-        })
-    })
+      this$1.authInstance.signOut().then(function () {
+        this$1._clearStorage();
+        this$1.authenticated = false;
+        res();
+      });
+    });
   }
 
   /**
@@ -158,9 +159,9 @@ export default class GoogleAuthService {
    * @return Boolean
    *
    */
-  isAuthenticated () {
-    const expiresAt = JSON.parse(localStorage.getItem('gapi.expires_at'))
-    return new Date().getTime() < expiresAt
+  isAuthenticated() {
+    const expiresAt = JSON.parse(localStorage.getItem("gapi.expires_at"));
+    return new Date().getTime() < expiresAt;
   }
 
   /**
@@ -173,10 +174,10 @@ export default class GoogleAuthService {
    * @return Boolean
    *
    */
-  isSignedIn () {
-    if (!this.authInstance) throw new Error('gapi not initialized')
-    const GoogleUser = this.authInstance.currentUser.get()
-    return GoogleUser.isSignedIn()
+  isSignedIn() {
+    if (!this.authInstance) throw new Error("gapi not initialized");
+    const GoogleUser = this.authInstance.currentUser.get();
+    return GoogleUser.isSignedIn();
   }
 
   /**
@@ -192,13 +193,13 @@ export default class GoogleAuthService {
    * @return Boolean. False if NOT authenticated, UserData if authenticated
    *
    */
-  listenUserSignIn (callback) {
-    if (!this.authInstance) throw new Error('gapi not initialized')
-    this.authInstance.isSignedIn.listen(callback)
+  listenUserSignIn(callback) {
+    if (!this.authInstance) throw new Error("gapi not initialized");
+    this.authInstance.isSignedIn.listen(callback);
     if (this.authInstance.currentUser.get().isSignedIn()) {
-      return this.getUserData()
+      return this.getUserData();
     } else {
-      return false
+      return false;
     }
   }
 
@@ -211,17 +212,17 @@ export default class GoogleAuthService {
    *
    * @return object with user data from localStorage
    */
-  getUserData () {
+  getUserData() {
     return {
-      id: localStorage.getItem('gapi.id'),
-      firstName: localStorage.getItem('gapi.first_name'),
-      lastName: localStorage.getItem('gapi.last_name'),
-      fullName: localStorage.getItem('gapi.full_name'),
-      email: localStorage.getItem('gapi.email'),
-      imageUrl: localStorage.getItem('gapi.image_url'),
-      expiresAt: localStorage.getItem('gapi.expires_at'),
-      accessToken: localStorage.getItem('gapi.access_token'),
-      idToken: localStorage.getItem('gapi.id_token')
-    }
+      id: localStorage.getItem("gapi.id"),
+      firstName: localStorage.getItem("gapi.first_name"),
+      lastName: localStorage.getItem("gapi.last_name"),
+      fullName: localStorage.getItem("gapi.full_name"),
+      email: localStorage.getItem("gapi.email"),
+      imageUrl: localStorage.getItem("gapi.image_url"),
+      expiresAt: localStorage.getItem("gapi.expires_at"),
+      accessToken: localStorage.getItem("gapi.access_token"),
+      idToken: localStorage.getItem("gapi.id_token"),
+    };
   }
 }
