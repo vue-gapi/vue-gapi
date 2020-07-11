76d8dae739aff03b236e268086fb261caf7b201c
Nick Amoscato <nick@amoscato.com>
2020-07-11 12:40:49 -0400

    Cleanup ESLint, Prettier configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,18 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  extends: 'vue',
-  // add your custom rules here
-  'rules': {
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'prettier',
+  ],
   globals: {
     requestAnimationFrame: true,
     performance: true,
-    window: true
+    window: true,
   },
-  "env": {
-    "browser": true,
-    "node": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
   },
 }
