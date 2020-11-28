88ae76055a577079ebfa21f1a8e773b2f3f15805
deCodeIt <shaikhali.jafri@iitrpr.ac.in>
2020-11-29 00:03:38 +0530

    Return promise for refreshToken so that caller can wait for the token to be refreshed.

--- src/VueGapi/GoogleAuthService.js
+++ src/VueGapi/GoogleAuthService.js
@@ -204,6 +204,8 @@ export default class GoogleAuthService {
    * @method GoogleAuthService#refreshToken
    * @see [GoogleUser.reloadAuthResponse]{@link https://developers.google.com/identity/sign-in/web/reference#googleuserreloadauthresponse}
    *
+   * @return {Promise}
+   *
    * @example
    * <script>
    *     name: 'App'
@@ -222,8 +224,16 @@ export default class GoogleAuthService {
   refreshToken() {
     if (!this.authInstance) throw new Error('gapi not initialized')
     const GoogleUser = this.authInstance.currentUser.get()
-    GoogleUser.reloadAuthResponse().then((authResult) => {
-      this._setStorage(authResult)
+    return new Promise((res, rej) => {
+      GoogleUser.reloadAuthResponse()
+        .then((authResult) => {
+          this._setStorage(authResult)
+          res()
+        })
+        .catch((error) => {
+          console.error(error)
+          rej(error)
+        })
     })
   }
 
