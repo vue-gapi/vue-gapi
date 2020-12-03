ebad21696e09cfd747aaefefa618f0a4a3962a3d
deCodeIt <shaikhali.jafri@iitrpr.ac.in>
2020-12-03 21:33:14 +0530

    Enhance testing for grant using describe and nested-it

--- test/GoogleAuthService.test.js
+++ test/GoogleAuthService.test.js
@@ -15,51 +15,59 @@ it('_expiresAt returns string of numbers only', () => {
   expect(res).toEqual(expect.stringMatching(/^[0-9]*$/))
 })
 
-it('grant() returns correct response', async () => {
-  const newService = new AuthService()
-  const gAuth = new GoogleAuthMock()
-  newService.authInstance = gAuth
-  newService.clientConfig = {
-    scope: 'scope1',
-  }
-  // Test that onResolve callback works.
-  let res = await new Promise((res) => {
-    newService.grant(
-      (googleUser) => res(googleUser),
-      (error) => {
-        console.log(error)
-        expect(false).toBeTruthy()
-      }
-    )
-  })
-  expect(res).toBeInstanceOf(GoogleUserMock)
+describe('grant() returns correct response', () => {
+  let newService
+  let gAuth
 
-  // Test that onReject callback as well as the promise is rejected works.
-  gAuth.currentUser.rejectGrant('access_denied')
-  res = await new Promise((res) => {
-    newService
-      .grant(
-        () => {
-          expect(false).toBeTruthy()
-        },
+  beforeEach(async () => {
+    newService = new AuthService()
+    gAuth = new GoogleAuthMock()
+    newService.authInstance = gAuth
+    newService.clientConfig = {
+      scope: 'scope1',
+    }
+  })
+
+  it('Test that onResolve callback works', async () => {
+    let res = await new Promise((res) => {
+      newService.grant(
+        (googleUser) => res(googleUser),
         (error) => {
+          console.log(error)
+          expect(false).toBeTruthy()
+        }
+      )
+    })
+    expect(res).toBeInstanceOf(GoogleUserMock)
+  })
+
+  it('Test that onReject callback as well as the promise is rejected works', async () => {
+    gAuth.currentUser.rejectGrant('access_denied')
+    let res = await new Promise((res) => {
+      newService
+        .grant(
+          () => {
+            expect(false).toBeTruthy()
+          },
+          (error) => {
+            expect(error).toMatchObject({
+              error: 'access_denied',
+            })
+            res()
+          }
+        )
+        .catch((error) => {
           expect(error).toMatchObject({
             error: 'access_denied',
           })
           res()
-        }
-      )
-      .catch((error) => {
-        expect(error).toMatchObject({
-          error: 'access_denied',
         })
-        res()
-      })
+    })
+    expect(res).toBeUndefined()
   })
-  expect(res).toBeUndefined()
 
-  // Test that the promise is also returned.
-  gAuth.currentUser.resolveGrant()
-  res = await newService.grant()
-  expect(res).toBeInstanceOf(GoogleUserMock)
+  it('Test that the promise is also returned', async () => {
+    let res = await newService.grant()
+    expect(res).toBeInstanceOf(GoogleUserMock)
+  })
 })
