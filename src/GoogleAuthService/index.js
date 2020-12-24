3f865a293c6d812fb34d192286a4f752127ac349
Nick Amoscato <nick@amoscato.com>
2020-12-24 09:23:46 -0500

    Add TypeScript type definitions
    Resolves #59

--- src/GoogleAuthService/index.js
+++ src/GoogleAuthService/index.js
@@ -165,7 +165,7 @@ export default class GoogleAuthService {
    * @method GoogleAuthService#refreshToken
    * @see [GoogleUser.reloadAuthResponse]{@link https://developers.google.com/identity/sign-in/web/reference#googleuserreloadauthresponse}
    *
-   * @return {Promise}
+   * @return {Promise<AuthResponse>}
    *
    * @example
    * <script>
@@ -297,7 +297,6 @@ export default class GoogleAuthService {
 
   /**
    * Accept the callback to be notified when the authentication status changes.
-   * Will also determine if the login token is valid using google methods and return UserData or false
    *
    * @method GoogleAuthService#listenUserSignIn
    * @see [GoogleAuth.isSignedIn.listen]{@link https://developers.google.com/identity/sign-in/web/reference#googleauthissignedinlistenlistener}
@@ -327,9 +326,10 @@ export default class GoogleAuthService {
    * @property {string} fullName full name
    * @property {string} email
    * @property {string} imageUrl
-   * @property {string} expiresAt
+   * @property {number} expiresAt
    * @property {string} accessToken granted access token
    * @property {string} idToken granted ID token
+   * @property {string} [offlineAccessCode]
    */
 
   /**
