00837fc5ea0ebc7772b69b889a13712a06469772
namoscato <namoscato@users.noreply.github.com>
2022-05-08 22:34:55 +0000

    deploy: 815a4037fbeaf86838af61f99df81844b48c527e

---
+++ assets/index.html.dc3c2723.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o,a,d as c}from"./app.9479e95c.js";const s={},_={id:"welcome-to-api",tabindex:"-1"},r=a("a",{class:"header-anchor",href:"#welcome-to-api","aria-hidden":"true"},"#",-1),n=c(" Welcome to API"),d=[r,n];function i(h,l){return t(),o("h3",_,d)}var f=e(s,[["render",i],["__file","index.html.vue"]]);export{f as default};
